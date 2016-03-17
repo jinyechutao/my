1c34bf43a3240c2eeab2c6797f872d652fee3b17
jinyechutao <1010266032@qq.com>
2016-03-17 22:07:39 +0800

    复利计算3.0

---
+++ fulijisuan/src/fulijisuan/Logarithm.java
@@ -0,0 +1,10 @@
+package fulijisuan;
+
+public class Logarithm {
+
+	public static double log(double value, double base) {
+		// TODO 自动生成的方法存根
+		return  Math.log(value/Math.log(base));
+	}
+
+}
--- fulijisuan/src/fulijisuan/text1.java
+++ fulijisuan/src/fulijisuan/text1.java
@@ -3,7 +3,9 @@ package fulijisuan;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
@@ -14,7 +16,6 @@ import javax.swing.UIManager;
 import org.dyno.visual.swing.layouts.Constraints;
 import org.dyno.visual.swing.layouts.GroupLayout;
 import org.dyno.visual.swing.layouts.Leading;
-import org.dyno.visual.swing.layouts.Trailing;
 
 //VS4E -- DO NOT REMOVE THIS LINE!
 public class text1 extends JFrame {
@@ -33,51 +34,35 @@ public class text1 extends JFrame {
 	private JButton jButton2;
 	private JTextField jTextField3;
 	private JTextField jTextField4;
-	private JRadioButton jRadioButton0;
 	public text1() {
 		initComponents();
 	}
 
 	private void initComponents() {
 		setLayout(new GroupLayout());
-		add(getJLabel0(), new Constraints(new Leading(34, 12, 12), new Leading(18, 10, 10)));
-		add(getJLabel1(), new Constraints(new Leading(33, 12, 12), new Leading(64, 12, 12)));
-		add(getJLabel2(), new Constraints(new Leading(32, 12, 12), new Leading(113, 10, 10)));
-		add(getJButton0(), new Constraints(new Leading(9, 12, 12), new Leading(259, 29, 10, 10)));
-		add(getJButton2(), new Constraints(new Leading(126, 10, 10), new Leading(259, 12, 12)));
-		add(getJButton1(), new Constraints(new Leading(239, 10, 10), new Leading(259, 12, 12)));
-		add(getJLabel4(), new Constraints(new Leading(30, 12, 12), new Leading(200, 10, 10)));
-		add(getJLabel3(), new Constraints(new Leading(30, 12, 12), new Leading(158, 10, 10)));
-		add(getJRadioButton0(), new Constraints(new Trailing(12, 109, 171), new Leading(62, 12, 12)));
-		add(getJRadioButton1(), new Constraints(new Leading(246, 10, 10), new Leading(131, 10, 10)));
-		add(getJTextField0(), new Constraints(new Leading(91, 95, 10, 10), new Leading(18, 10, 10)));
-		add(getJTextField2(), new Constraints(new Leading(91, 94, 12, 12), new Leading(109, 12, 12)));
-		add(getJTextField1(), new Constraints(new Leading(93, 90, 88, 88), new Leading(64, 12, 12)));
-		add(getJTextField3(), new Constraints(new Leading(91, 94, 12, 12), new Leading(154, 12, 12)));
-		add(getJTextField4(), new Constraints(new Leading(90, 96, 12, 12), new Leading(198, 12, 12)));
-		setSize(320, 388);
-	}
-
-	private JRadioButton getJRadioButton1() {
-		if (jRadioButton1 == null) {
-			jRadioButton1 = new JRadioButton();
-			jRadioButton1.setSelected(false);
-			jRadioButton1.setText("算本金");
-			jRadioButton1.addMouseListener(new MouseAdapter() {
-	
-				public void mouseClicked(MouseEvent event) {
-					jRadioButton1MouseMouseClicked(event);
-				}
-			});
-		}
-		return jRadioButton1;
+		add(getJTextField4(), new Constraints(new Leading(123, 96, 10, 10), new Leading(200, 12, 12)));
+		add(getJTextField3(), new Constraints(new Leading(124, 94, 12, 12), new Leading(156, 12, 12)));
+		add(getJTextField2(), new Constraints(new Leading(124, 94, 12, 12), new Leading(111, 12, 12)));
+		add(getJTextField0(), new Constraints(new Leading(125, 95, 12, 12), new Leading(18, 12, 12)));
+		add(getJTextField1(), new Constraints(new Leading(125, 92, 12, 12), new Leading(64, 12, 12)));
+		add(getJLabel4(), new Constraints(new Leading(72, 12, 12), new Leading(202, 12, 12)));
+		add(getJLabel3(), new Constraints(new Leading(72, 12, 12), new Leading(158, 12, 12)));
+		add(getJButton0(), new Constraints(new Leading(30, 10, 10), new Leading(305, 29, 10, 10)));
+		add(getJButton1(), new Constraints(new Leading(137, 10, 10), new Leading(305, 12, 12)));
+		add(getJButton2(), new Constraints(new Leading(220, 12, 12), new Leading(305, 12, 12)));
+		add(getJLabel1(), new Constraints(new Leading(70, 12, 12), new Leading(68, 13, 12, 12)));
+		add(getJLabel2(), new Constraints(new Leading(72, 12, 12), new Leading(111, 12, 12)));
+		add(getJLabel0(), new Constraints(new Leading(70, 12, 12), new Leading(20, 12, 12)));
+		add(getJRadioButton0(), new Constraints(new Leading(259, 10, 10), new Leading(120, 12, 12)));
+		add(getJComboBox0(), new Constraints(new Leading(266, 10, 10), new Leading(50, 12, 12)));
+		setSize(395, 388);
 	}
 
 	private JRadioButton getJRadioButton0() {
 		if (jRadioButton0 == null) {
 			jRadioButton0 = new JRadioButton();
 			jRadioButton0.setSelected(false);
-			jRadioButton0.setText("算存款");
+			jRadioButton0.setText("每年投入年金");
 			jRadioButton0.addMouseListener(new MouseAdapter() {
 	
 				public void mouseClicked(MouseEvent event) {
@@ -86,6 +71,18 @@ public class text1 extends JFrame {
 			});
 		}
 		return jRadioButton0;
+	}
+
+	private JComboBox getJComboBox0() {
+		if (jComboBox0 == null) {
+			jComboBox0 = new JComboBox();
+			jComboBox0.setModel(new DefaultComboBoxModel(new Object[] { "算存款", "算本金", "算利率", "算年限" }));
+			jComboBox0.setDoubleBuffered(false);
+			jComboBox0.setBorder(null);
+			jComboBox0.addMouseListener(new MouseAdapter() {
+			});
+		}
+		return jComboBox0;
 	}
 
 	private JTextField getJTextField4() {
@@ -215,15 +212,11 @@ public class text1 extends JFrame {
 			System.err.println("Cannot install " + PREFERRED_LOOK_AND_FEEL + " on this platform:" + e.getMessage());
 		}
 	}
-
-	/**
-	* Main entry of the class.
-	* Note: This class is only created so that you can easily preview the result at runtime.
-	* It is not expected to be managed by the designer.
-	* You can modify it as you like.
-	*/
-	boolean suanfa = true ;
-	private JRadioButton jRadioButton1;
+	boolean y = false;
+	private JComboBox jComboBox0;
+	private JTextField jTextField5;
+	private JLabel jLabel5;
+	private JRadioButton jRadioButton0;
 	private static final String PREFERRED_LOOK_AND_FEEL = "javax.swing.plaf.metal.MetalLookAndFeel";
 	public static void main(String[] args) {
 		installLnF();
@@ -241,24 +234,37 @@ public class text1 extends JFrame {
 	}
 
 	private void jButton0MouseMouseClicked(MouseEvent event) {
+		String key;
 		double p=0,i=0,f=0,j=0; 
 		double s=1; 
-		 int n=0; 		 
-		 i = Double.parseDouble(jTextField1.getText()); 
-		 n = Integer.parseInt(jTextField2.getText());
-		 if(suanfa == false)
+		double n=0;  
+		 key = (String) jComboBox0.getSelectedItem();
+		 if(key == "算存款")
 		 {
+			 i = Double.parseDouble(jTextField1.getText()); 
+		 n = Double.parseDouble(jTextField2.getText());
 			 p = Double.parseDouble(jTextField0.getText()); 
-
-		 for(int a = 0;a < n;a++){ 
+			 if(y==true){
+		 for(int a = 0;a < n;a++){
+			 p = p+f;
+		 f = p * (1 + i); 
+		 } 
+		 j = f - p;  
+			 }
+			 else{
+				 for(int a = 0;a < n;a++){ 
+			
 		 s = s * (1 + i); 
 		 } 
 		 f =  p * s; 
-		 j = f - p;  
+		 j = f - p;
+		 }
 		 this.jTextField3.setText(String.format("%.2f", f)); 
 		 this.jTextField4.setText(String.format("%.2f", j));
 		 }
-		 else{
+		 else if(key == "算本金"){	
+			 i = Double.parseDouble(jTextField1.getText()); 
+		 n = Double.parseDouble(jTextField2.getText());
 			 f = Double.parseDouble(jTextField3.getText()); 
 			 for(int a = 0;a<n;a++){
 				 s= (i+1)*s;
@@ -267,6 +273,24 @@ public class text1 extends JFrame {
 			 j=f-p;
 			 
 			this.jTextField0.setText(String.format("%.2f", p)); 
+		 this.jTextField4.setText(String.format("%.2f", j));
+		 }			
+		 else if(key == "算利率"){	
+			 p = Double.parseDouble(jTextField0.getText()); 
+		 n = Double.parseDouble(jTextField2.getText());
+			 f = Double.parseDouble(jTextField3.getText()); 
+			i = Math.pow(f/p, 1.0/n)-1;
+			 j=f-p;			 
+			this.jTextField1.setText(String.format("%.2f", i)); 
+		 this.jTextField4.setText(String.format("%.2f", j));
+		 }
+		 else if(key == "算年限"){	
+			 p = Double.parseDouble(jTextField0.getText()); 
+			 i = Double.parseDouble(jTextField1.getText());
+			 f = Double.parseDouble(jTextField3.getText()); 
+			 n = Logarithm.log(f/p, 1+i); 
+			 j=f-p;			 
+			this.jTextField2.setText(String.format("%.2f", n)); 
 		 this.jTextField4.setText(String.format("%.2f", j));
 		 }
 	}
@@ -280,36 +304,56 @@ public class text1 extends JFrame {
 	}
 
 	private void jButton2MouseMouseClicked(MouseEvent event) {
-		double p=0,i=0,j=0,f=0;;
-		 int n=0; 
-
-		 i = Double.parseDouble(jTextField1.getText()); 
-		 n = Integer.parseInt(jTextField2.getText());
-		 if(suanfa == false){
+		double p=0,i=0,j=0,f=0,n=0;  
+		 String key;
+		 key = (String) jComboBox0.getSelectedItem();
+		 if(key == "算存款"){
+			 i = Double.parseDouble(jTextField1.getText()); 
+		 n = Double.parseDouble(jTextField2.getText());
 		 p = Double.parseDouble(jTextField0.getText()); 
-		 j=p*i*n;
+		 if(y==true)
+			 j=p*n*i*n;
+		 else
+			 j=p*i*n;
 		 f=j+p;  
 		 this.jTextField3.setText(String.format("%.2f", f)); 
 		 this.jTextField4.setText(String.format("%.2f", j));
 		 }
-		 else{
+		 else if(key == "算本金"){	
+			 i = Double.parseDouble(jTextField1.getText()); 
+		 n = Double.parseDouble(jTextField2.getText());
 			 f = Double.parseDouble(jTextField3.getText()); 
-			 p = f *(1/(1+i*n));
+			 p = f *(1/(1+i*n)); 
 			 j=f-p;
 			 
 			this.jTextField0.setText(String.format("%.2f", p)); 
 		 this.jTextField4.setText(String.format("%.2f", j));
+		 }	
+		 else if(key == "算利率"){	
+			 p = Double.parseDouble(jTextField0.getText()); 
+		 n = Double.parseDouble(jTextField2.getText());
+			 f = Double.parseDouble(jTextField3.getText()); 
+			 i = (f-p)/ (p*n);
+			 j=f-p;			 
+			this.jTextField1.setText(String.format("%.2f", i)); 
+			this.jTextField4.setText(String.format("%.2f", j));
+		 }
+		 else if(key == "算年限"){	
+			 p = Double.parseDouble(jTextField0.getText()); 
+			 i = Double.parseDouble(jTextField1.getText());
+			 f = Double.parseDouble(jTextField3.getText()); 
+			 n = (f-p)/(p*i);
+			 j=f-p;			 
+			this.jTextField2.setText(String.format("%.2f", n)); 
+			this.jTextField4.setText(String.format("%.2f", j));
 		 }
 	}
 
 	private void jRadioButton0MouseMouseClicked(MouseEvent event) {
-		suanfa = false;
-		jRadioButton1.setSelected(false);
-	}
-
-	private void jRadioButton1MouseMouseClicked(MouseEvent event) {
-		jRadioButton0.setSelected(false);
-		suanfa = true;
+		if(y==false)
+			y = true;
+		else
+			y=false;
 	}
 
 }
